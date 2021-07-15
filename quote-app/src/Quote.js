a3e808fd69831bb6127b2bdb7fa0ef695dcf6998
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2021-07-15 12:26:43 +0545

    Random Bug Fixed

--- quote-app/src/Quote.js
+++ quote-app/src/Quote.js
@@ -17,9 +17,9 @@ const {name,job,text,image}=QuoteData[index]
             <p className="job">{job}</p>
             <p className="info">{text}</p>
             <div className="button-container">
-                <button className="prev-btn" onClick={()=>index===0?setIndex(index.length-1):setIndex(index-1)}><i className="fa fa-angle-left"></i></button>
-                <button className="next-btn"  onClick={()=>index===index.length-1?setIndex(0):setIndex(index+1)}><i className="fa fa-angle-right"></i></button>
-                <button className="random-btn" onClick={()=>setIndex(Math.floor(Math.random() * 5))}>Random</button>
+                <button className="prev-btn" onClick={()=>index===0?setIndex(QuoteData.length-1):setIndex(index-1)}><i className="fa fa-angle-left"></i></button>
+                <button className="next-btn"  onClick={()=>index===QuoteData.length-1-1?setIndex(0):setIndex(index+1)}><i className="fa fa-angle-right"></i></button>
+                <button className="random-btn" onClick={()=>setIndex(Math.floor(Math.random() * (QuoteData.length)))}>Random</button>
             </div>
         </article>
     )
